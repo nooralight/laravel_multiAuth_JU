eaef8c50a3f488cffab037d19107aa4105c4f629
nooralight <2017100000041@seu.edu.bd>
2022-01-31 17:16:38 +0600

    ini

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,4 +12,8 @@ class AdminController extends Controller
         $users= User::all();
         return view('admin')->with(["users"=>$users]);
     }
+    public function update($id){
+        $status = User::find($id);
+        return view('home',compact('status'));
+    }
 }
